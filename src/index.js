16ef18b7bbb81268da9ef52ad2fc9e6e756be925
VickyRong <515020842@qq.com>
2020-07-30 14:54:17 +0800

    引入react-router-dom 添加路由

--- src/index.js
+++ src/index.js
@@ -1,44 +1,54 @@
 /**
- * 基础引入
- */
-//  import _ from 'lodash';
-
-//   function component() {
-//     var element = document.createElement('div');
-//     element.innerHTML = _.join(['Hello', 'huyanrong!!!',], ' ');
-//     element.classList.add('hello');
-//     return element;
-//   }
-
-//   document.body.appendChild(component());
-
-
-/**
  * 引入JSX语法
  */
 import './style/index.css';
 import React from 'react';
+import { BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+ } from 'react-router-dom'
+
 import ReactDOM from 'react-dom';
 import TreeDemo from './demo/tree-demo'
 import ButtonDemo from './demo/button-demo'
 import MockDemo from './demo/mock-demo'
 import RequestDemo from './demo/request-demo'
+import FormPage from './pages/form/index'
+
+const NavList = 
+<>
+  <ul>
+    <li><Link to="/tree">树结构</Link></li>
+    <li><Link to="/button">按钮</Link></li>
+    <li><Link to="/mock">数据模拟示例</Link></li>
+    <li><Link to="/request">请求示例</Link></li>
+    <li><Link to="/form">表单页面</Link></li>
+  </ul>
+</>;
+
 
 ReactDOM.render(
-  <div className="container">
-    <div className="g-mb-20">
-      <ButtonDemo/>
-    </div>
-    <div className="g-mb-20">
-      <TreeDemo/>
-    </div>
-    <div className="g-mb-20">
-      <MockDemo/>
-    </div>
-    <div className="g-mb-20">
-      <RequestDemo/>
-    </div>
-  </div>,
+  <Router>
+        { NavList }
+        <Switch>
+          <Route path="/tree">
+            <TreeDemo />
+          </Route>
+          <Route path="/button">
+            <ButtonDemo />
+          </Route>
+          <Route path="/mock">
+            <MockDemo />
+          </Route>
+          <Route path="/request">
+            <RequestDemo />
+          </Route>
+          <Route path="/form">
+            <FormPage />
+          </Route>
+        </Switch>
+  </Router>,
   document.querySelector('#root'),
 );
 
---
+++ src/pages/form/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class FormPage extends  React.Component{
+    render(){
+        return <>
+            <a href="#">表单组件</a>
+        </>
+    }
+}
+
+export default FormPage;
